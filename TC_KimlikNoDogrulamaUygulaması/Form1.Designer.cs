ea8310d315ebf4feae5426b6d42888111b1b57d3
Nazım Hüseyin Özen <hozen343@gmail.com>
2025-03-13 22:21:30 +0300

    Add project files.

---
+++ TC_KimlikNoDogrulamaUygulaması/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TC_KimlikNoDogrulamaUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelyil = new System.Windows.Forms.Label();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.lblSoyisim = new System.Windows.Forms.Label();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSorgula = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelyil);
+            this.groupBox1.Controls.Add(this.txtYil);
+            this.groupBox1.Controls.Add(this.lblSoyisim);
+            this.groupBox1.Controls.Add(this.txtSoyisim);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtIsim);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTC);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(246, 286);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi Panel";
+            // 
+            // labelyil
+            // 
+            this.labelyil.AutoSize = true;
+            this.labelyil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelyil.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelyil.Location = new System.Drawing.Point(6, 221);
+            this.labelyil.Name = "labelyil";
+            this.labelyil.Size = new System.Drawing.Size(75, 17);
+            this.labelyil.TabIndex = 7;
+            this.labelyil.Text = "Doğum Yılı";
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(6, 244);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(219, 29);
+            this.txtYil.TabIndex = 6;
+            // 
+            // lblSoyisim
+            // 
+            this.lblSoyisim.AutoSize = true;
+            this.lblSoyisim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSoyisim.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSoyisim.Location = new System.Drawing.Point(6, 161);
+            this.lblSoyisim.Name = "lblSoyisim";
+            this.lblSoyisim.Size = new System.Drawing.Size(56, 17);
+            this.lblSoyisim.TabIndex = 5;
+            this.lblSoyisim.Text = "Soyisim";
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(6, 184);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(219, 29);
+            this.txtSoyisim.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(6, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İsim";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(6, 122);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(219, 29);
+            this.txtIsim.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "T.C Kimlık Numarası";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(6, 61);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(219, 29);
+            this.txtTC.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSorgula);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(264, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(169, 286);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlem Panel";
+            // 
+            // btnSorgula
+            // 
+            this.btnSorgula.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSorgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgula.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSorgula.Location = new System.Drawing.Point(6, 36);
+            this.btnSorgula.Name = "btnSorgula";
+            this.btnSorgula.Size = new System.Drawing.Size(157, 35);
+            this.btnSorgula.TabIndex = 0;
+            this.btnSorgula.Text = "SORGULA";
+            this.btnSorgula.UseVisualStyleBackColor = false;
+            this.btnSorgula.Click += new System.EventHandler(this.btnSorgula_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(445, 310);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T.C Kimlik No Doğrulama";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSorgula;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelyil;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.Label lblSoyisim;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIsim;
+    }
+}
+
---
+++ TC_KimlikNoDogrulamaUygulaması/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TC_KimlikNoDogrulamaUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSorgula_Click(object sender, EventArgs e)
+        {
+
+            long tc = long.Parse(txtTC.Text);
+            string isim = txtIsim.Text.ToUpper();
+            string soyisim = txtSoyisim.Text.ToUpper();
+            int yil = int.Parse(txtYil.Text);
+
+
+
+            TCKimlikDogrulama.KPSPublic kps = new TCKimlikDogrulama.KPSPublic();
+
+            bool control = kps.TCKimlikNoDogrula(tc, isim, soyisim, yil);
+
+            if (control)
+            {
+                MessageBox.Show("Bilgileriniz doğrulandı !", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Bilgileriniz HATALI !", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+    }
+}
